debddcd641fa862f8385a562ce8a4003d8dd27b5
lightbell03 <bellight28@gmail.com>
2025-02-07 17:50:38 +0900

    opentelemetry docker 환경 구성

---
+++ fluentd/script/log_generator.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+touch /script/log.txt
+
+while true :
+do
+    echo "hello" >> /script/log.txt
+    sleep 3
+done
---
+++ node_exporter/script/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+START=0
+
+END=1000000000000000000
+
+        while [ $START -le $END ]
+
+        do
+
+        START=`expr $START + 1`
+
+done
